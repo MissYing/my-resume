936d56059f8d2ca078075587555a7f8335be7995
ying <ying@yingdeMacBook-Pro.local>
2017-07-21 17:30:01 +0800

    used express

---
+++ server/index.js
@@ -0,0 +1,5 @@
+// 引入api
+const api = require('./api');
+//引入文件模块
+const fs = require('fs');
+//引入处理路径的
--- src/frame/index.vue
+++ src/frame/index.vue
@@ -1,6 +1,14 @@
 <template>
 	<div class="ying-index">
-		<PublicLeft :userInfo="userInfo"></PublicLeft>
+		<ul class="public_left">
+	    	<li><img src="../assets/images/user.jpg"></li>
+	    	<li class="name">{{ userInfo.name }}</li>
+	    	<li class="intention">求职意向:移动前端/web前端</li>
+	    	<li class="address">上海</li>
+	    	<li class="phone">{{ userInfo.phone }}</li>
+	    	<li class="email">{{ userInfo.email }}</li>
+	    	<li class="github"><a :href="userInfo.github">{{ userInfo.github }}</a></li>
+	    </ul>
 		<div class="public-right">
 		    <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect">
 			  	<el-menu-item index="1"><router-link to="/baseInfo">基本信息</router-link></el-menu-item>
@@ -14,7 +22,6 @@
 </template>
 
 <script>
-import PublicLeft from '../pages/PublicLeft.vue'
 
 export default {
 	name: 'Index',
@@ -32,7 +39,6 @@ export default {
 	    this.getUserInfo()
 	},
 	components: {
-		PublicLeft
 	},
 	methods: {
 		handleSelect(key, keyPath) {
@@ -89,6 +95,60 @@ export default {
 		margin: auto;
 		padding-top: 1.25rem;
 		position: relative;
+		.public_left {
+			background-color: #42b983;
+			padding: .625rem .9375rem;
+			color: #fff;
+			width: 11.5625rem;
+			min-height: 31.25rem;
+			float: left;
+			>li {
+				position: relative;
+				padding: .625rem 0;
+				img {
+					width: 6rem;
+					height: auto;
+					display: block;
+					margin: auto;
+				}
+				a {
+					color: #fff;
+					word-break: break-all; /*支持IE，chrome，FF不支持*/
+		    		word-wrap: break-word; /*支持IE，chrome，FF*/
+				}
+			}
+			.address:before, .phone:before, .email:before, .github:before {
+				content: "";
+				display: inline-block;
+				vertical-align: middle;
+				margin-right: .5rem;
+				width: 1rem;
+				height: 1rem;
+				@include bg();
+			}
+			.address:before {
+				background-image: url('../assets/images/address.png');
+			}
+			.phone:before {
+				background-image: url('../assets/images/phone.png');
+			}
+			.email:before {
+				background-image: url('../assets/images/email.png');
+			}
+			.github:before {
+				background-image: url('../assets/images/github.png');
+			}
+			.name {
+				@include fontSize(1.5rem);
+				text-align: center;
+			}
+			.intention {
+				@include fontSize(.9rem);
+				border-top: .01rem solid #e8e8e8;
+				color: #1f1809;
+				@include paddingB(3rem);
+			}
+		}
 		.public-right {
 			margin-left: 14.375rem;
 			.el-menu {
